826a3f4b2f56a8727244d08ff7090f39ff61d683
mikelinux <mhigham1986@gmail.com>
2015-08-09 20:15:33 +0200

    Added new file for functions

---
+++ functions.h
@@ -0,0 +1,19 @@
+/*
+ *	fbtruetype-android
+ *
+ *	(c) 2015 by Michael Higham, <mhigham1986@gmail.com>
+ *  
+ *	This program is based on fbtruetype, written and (C) by
+ *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
+ *
+ *	This program my be redistributed under the terms of the
+ *	GNU General Public Licence, version 2, or at your preference,
+ *	any later version.
+ */
+
+#ifndef __FUNCTIONS_H
+#define __FUNCTIONS_H
+
+extern int strwrap(char * s, int w, char *** line_ret, int ** len_ret);
+
+#endif
