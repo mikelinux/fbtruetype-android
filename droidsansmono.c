4ce0a17fab630575f2e3accd3613bb9184da746e
mikelinux <mhigham1986@gmail.com>
2015-08-09 18:32:12 +0200

    Cleaned code

--- droidsansmono.c
+++ droidsansmono.c
@@ -1,3 +1,15 @@
+/*
+ *	fbtruetype-android
+ *
+ *	(c) 2015 by Michael Higham, <mhigham1986@gmail.com>
+ *  
+ *	This program is based on fbtruetype, written and (C) by
+ *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
+ *
+ *	This program my be redistributed under the terms of the
+ *	GNU General Public Licence, version 2, or at your preference,
+ *	any later version.
+ */
 #define DROIDSANSMONO_SIZE 118640
 
 char DroidSansMono_ttf[DROIDSANSMONO_SIZE] = {
