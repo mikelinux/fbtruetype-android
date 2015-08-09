4a544ea5ad369af8487363108ac2a7a3ec96a2d3
mikelinux <mhigham1986@gmail.com>
2015-08-09 18:34:27 +0200

    Code cleaned up

--- ttf.h
+++ ttf.h
@@ -1,3 +1,16 @@
+/*
+ *	fbtruetype-android
+ *
+ *	(c) 2015 by Michael Higham, <mhigham1986@gmail.com>
+ *  
+ *	This program is based on fbtruetype, written and (C) by
+ *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
+ *
+ *	This program my be redistributed under the terms of the
+ *	GNU General Public Licence, version 2, or at your preference,
+ *	any later version.
+ */
+
 #define CACHED_METRICS  0x10
 #define CACHED_BITMAP   0x01
 #define CACHED_PIXMAP   0x02
