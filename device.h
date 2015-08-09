df12e22d2e94899261825cc4a4d839cda4fd8a64
mikelinux <mhigham1986@gmail.com>
2015-08-09 18:31:45 +0200

    Added new options

--- device.h
+++ device.h
@@ -1,10 +1,10 @@
 /*
- *	fbmngplay - fb console MNG player.
- *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
+ *	fbtruetype-android
+ *
+ *	(c) 2015 by Michael Higham, <mhigham1986@gmail.com>
  *  
- *	This program is based on mngplay, part of libmng, written and (C) by
- *	
- *	Ralph Giles <giles@ashlu.bc.ca>
+ *	This program is based on fbtruetype, written and (C) by
+ *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
  *
  *	This program my be redistributed under the terms of the
  *	GNU General Public Licence, version 2, or at your preference,
@@ -14,8 +14,12 @@
 #ifndef __DEVICE_H
 #define __DEVICE_H
 
-extern void clear_zygote(void);
 extern void clear_display(void);
-extern void prepare_display(void);
+extern void power_key(void);
+extern void power_status(void);
+extern void power_display(void);
+extern void set_brightness(int brightness, int blen);
+extern void wait_sleep(void);
+extern void wait_wake(void);
 
 #endif
