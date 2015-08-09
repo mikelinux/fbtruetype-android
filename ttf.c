323977906c173b1a50d36085f22fea122c0c5553
mikelinux <mhigham1986@gmail.com>
2015-08-09 18:34:22 +0200

    Code cleaned up

--- ttf.c
+++ ttf.c
@@ -1,7 +1,14 @@
-/* render true type fonts to fb0 
+/*
+ *	fbtruetype-android
  *
- * (w) by michael@higham, code reused from SDL_ttf
+ *	(c) 2015 by Michael Higham, <mhigham1986@gmail.com>
+ *  
+ *	This program is based on fbtruetype, written and (C) by
+ *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
  *
+ *	This program my be redistributed under the terms of the
+ *	GNU General Public Licence, version 2, or at your preference,
+ *	any later version.
  */
 
 #include <math.h>
