989ff6ceeff91c587f9578785de5ff332603fe1d
mikelinux <mhigham1986@gmail.com>
2015-08-09 18:32:35 +0200

    New options and cleanup

--- fbtruetype.c
+++ fbtruetype.c
@@ -1,9 +1,10 @@
 /*
- *	fbmngplay - fb console MNG player.
- *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
+ *	fbtruetype-android
+ *
+ *	(c) 2015 by Michael Higham, <mhigham1986@gmail.com>
  *  
- *	This program is based on mngplay, written and (C) by
- *	Ralph Giles <giles@ashlu.bc.ca>
+ *	This program is based on fbtruetype, written and (C) by
+ *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
  *
  *	This program my be redistributed under the terms of the
  *	GNU General Public Licence, version 2, or at your preference,
@@ -28,7 +29,6 @@
 #include "console.h"
 
 volatile int run = 1;
-int verbose = 0;
 int buffered = 0;
 int waitsignal = 0;
 int delta = 16;
@@ -42,6 +42,8 @@ unsigned char *framebuffer, *font=DEFAULT_FONTNAME;
 unsigned int fontcolor=0xffffff;
 unsigned int fontsize=12;
 int boxwidth=65;
+int blen=3;
+int brightness=170;
 int strict_font=0;
 int rendertext(char *text, char *fontname, unsigned int size, unsigned int forecol);
 
@@ -143,7 +145,8 @@ int strwrap(char * s, int w, char *** line_ret, int ** len_ret)
 
 int main(int argc, char *argv[])
 {
-  	char textstr[1024];
+  	char *textstr;
+	char *stringraw;
   	char ** line;
   	int * len;
   	int lines;
@@ -155,82 +158,97 @@ int main(int argc, char *argv[])
 	struct fb_var_screeninfo var;
 	struct fb_fix_screeninfo fix;
 
-	/* Check which console we're running on */
+	/* Initialize consoles */
 	init_consoles();
 		
 	alpha = 100;
 	while (1) {
 		static struct option long_options[] = {
-			{"zygote", 0, 0, 'd'},
-			{"clear", 0, 0, 'z'},
-			{"help", 0, 0, 'h'},
-			{"verbose", 0, 0, 'v'},
-			{"alpha", 1, 0, 'a'},
-			{"boxwidth", 1, 0, 'b'},
-			{"version", 0, 0, 'V'},
-			{"start-console",0,0,'S'},
-			{"size",1,0,'s'},
-			{"console",1,0,'c'}, 
-			{"font",1,0,'f'}, 
-			{"textcolor",1,0,'t'},  
+            		{"help", no_argument, 0, 'h'},
+			{"version", no_argument, 0, 'V'},
+			{"info", no_argument, 0, 'i'},
+			{"zygote", no_argument, 0, 'z'},
+			{"power", no_argument, 0, 'p'},
+			{"clear", no_argument, 0, 'c'},
+			{"brightness", required_argument, 0, 'b'},
+			{"boxwidth", required_argument, 0, 'w'},
+			{"font", required_argument, 0, 'f'},
+			{"size", required_argument, 0, 's'},
+			{"textcolor", required_argument, 0, 't'},
+			{"alpha", required_argument, 0, 'a'},
 			{0, 0, 0, 0}
 		};
 
-		c = getopt_long(argc, argv, "a:b:x:y:dzh?vVSc:f:t:s:",
+		c = getopt_long(argc, argv, ":x:y:h?Vizpcb:w:f:s:t:a:",
 				long_options, &option_index);
 
 		if (c == -1)
 			break;
 
 		switch (c) {
-		case 'a':
-			alpha = atoi(optarg);
-			if (alpha > 100)
-				alpha = 100;
-			break;
-		case 'b':
-			boxwidth = atoi(optarg);
-			break;
 		case 'x':
 			fbxpos = atoi(optarg);
 			break;
 		case 'y':
 			fbypos = atoi(optarg);
 			break;
-		case 'd':
-			fprintf(stderr, "Zygote Mode.\n");
-			prepare_display();
-			fprintf(stderr, "Waiting 3 seconds...\n");
-			sleep(3);
-			clear_zygote();
-			break;
-		case 'z':
-			clear_display();
-			exit(0);
 		case '?':
 		case 'h':
 			usage(argv[0]);
 			exit(0);
-		case 'v':
-			verbose = 1;
-			break;
 		case 'V':
 			version();
 			exit(0);
+		case 'i':
+			display_info();
+			exit(0);
+		case 'z':
+			if (optind >= argc) {
+				printf("No text\n");
+				exit(0);
+			}
+			printf("Zygote Mode.\n");
+			printf("Waiting for display to sleep...\n");
+			wait_sleep();
+			power_display();
+			set_brightness(brightness,blen);
+			wait_wake();
+			clear_display();
+			break;
+		case 'p':
+			power_key();
+			exit(0);
 		case 'c':
-			start_console=atoi(optarg)-1;
-		case 'S':
-			sconly=1;
+			clear_display();
+			printf("Device display cleared.\n");
+			exit(0);
+		case 'b':
+			brightness = atoi(optarg);
+			if (brightness > 100 && brightness <= 255 ) {
+				blen = 3;
+			} else if (brightness <= 99 && brightness >= 10 ) {
+				blen = 2;
+			} else if (brightness <= 9 && brightness >= 0 ) {
+				blen = 1;
+			}
+			break;
+		case 'w':
+			boxwidth = atoi(optarg);
 			break;
 		case 'f':
 			strict_font=1;
 			font=strdup(optarg);
 			break;
+		case 's':
+			fontsize=strtol(optarg, NULL, 10);
+			break;
 		case 't':
 			fontcolor=strtol(optarg, NULL, 16);
 			break;
-		case 's':
-			fontsize=strtol(optarg, NULL, 10);
+		case 'a':
+			alpha = atoi(optarg);
+			if (alpha > 100)
+				alpha = 100;
 			break;
 		default:
 			break;
@@ -242,8 +260,11 @@ int main(int argc, char *argv[])
 	}
 	
 	
-	// Power on device display and set it to max brightness, then clear the framebuffer
-	prepare_display();
+	// Power on display
+	power_display();
+
+	// Set the brightness
+	set_brightness(brightness,blen);
 
 	// Initialize framebuffer
         fbdev = open("/dev/graphics/fb0", O_RDWR);
@@ -260,7 +281,8 @@ int main(int argc, char *argv[])
 	fblinelen = fix.line_length;
 	fbyposoff = fbypos;
 
-	lines = strwrap(argv[optind], boxwidth, &line, &len);
+	stringraw = argv[optind];
+	lines = strwrap(stringraw, boxwidth, &line, &len);
 	  for (i = 0; i < lines; i++)
 	  {
 	    if (i > 0)
@@ -273,7 +295,7 @@ int main(int argc, char *argv[])
     	    }
 	    strncpy(textstr, line[i], len[i]);
 	    textstr[len[i]] = '\0';
-	     //printf("%4d |%-*s|\n", len[i], boxwidth, textstr);
+	     	//printf("%4d |%-*s|\n", len[i], boxwidth, argv[optind]);
 	    framebuffer = mmap(NULL,  fblinelen* fby ,
 		 PROT_WRITE | PROT_READ, MAP_SHARED, fbdev, var.yoffset * fblinelen);
 	    rendertext (("%s", len[i], boxwidth, textstr), font, fontsize, fontcolor);
