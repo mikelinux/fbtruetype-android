19c3dfd48f06288de6688df4b78de2fdfeba0ddd
mikelinux <mhigham1986@gmail.com>
2015-08-09 18:32:40 +0200

    New options and cleanup

--- fbtruetype.h
+++ fbtruetype.h
@@ -1,10 +1,10 @@
 /*
- *	fbmngplay - fb console MNG player.
- *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
+ *	fbtruetype-android
+ *
+ *	(c) 2015 by Michael Higham, <mhigham1986@gmail.com>
  *  
- *	This program is based on mngplay, part of libmng, written and (C) by
- *	
- *	Ralph Giles <giles@ashlu.bc.ca>
+ *	This program is based on fbtruetype, written and (C) by
+ *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
  *
  *	This program my be redistributed under the terms of the
  *	GNU General Public Licence, version 2, or at your preference,
@@ -14,14 +14,13 @@
 #ifndef __FBTRUETYPE_H
 #define __FBTRUETYPE_H
 
-#define FBTRUETYPE_VERSION "0.3"
+#define FBTRUETYPE_VERSION "0.40"
 
 #ifndef DEFAULT_FONTNAME 
 #define DEFAULT_FONTNAME "/system/fonts/DroidSansMono.ttf"
 #endif 
 
 extern volatile int run;
-extern int verbose;
 extern int buffered;
 extern int dynpos;
 extern int waitsignal;
