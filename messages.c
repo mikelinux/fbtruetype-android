13c95eb01175720d805b6b8de8ccfa667f886845
mikelinux <mhigham1986@gmail.com>
2015-08-09 18:33:39 +0200

    Options cleanup/update

--- messages.c
+++ messages.c
@@ -1,6 +1,11 @@
 /*
+ *	fbtruetype-android
+ *
  *	(c) 2015 by Michael Higham, <mhigham1986@gmail.com>
  *  
+ *	This program is based on fbtruetype, written and (C) by
+ *	(c) 2001-2002 by Stefan Reinauer, <stepan@suse.de>
+ *
  *	This program my be redistributed under the terms of the
  *	GNU General Public Licence, version 2, or at your preference,
  *	any later version.
@@ -16,20 +21,22 @@ void usage(char *name)
 	fprintf(stderr, "\nUsage: %s [OPTION] [TEXT]\n", name);
 	fprintf(stderr,
 		"\nOptions:\n\n"
-		"	-x:			X coordinate pixel (default 10)\n"
-		"	-y:			Y coordinate pixel (default 10)\n"
-		"	-b, --boxwidth:		Text box column size (default 30)\n"
-		"	-f, --font:		Font name (.ttf file)\n"
-		"	-t, --textcolor:	Text color (RGB hex, e.g. 0xffffff)\n"
-                "	-s, --size:		Font size (default 22)\n"
-		"	-a, --alpha:		Alpha channel (1-100)\n"
-		"	-v, --verbose:		Verbose mode\n"
-		"	-V, --version:		Show version\n"
 		"	-?, -h, --help:		Print this help.\n"
-		"	-S, --start-console:	Output on start console only.\n"
-		"	-c:			Start on specified console.\n"
-		"	-d, --zygote:		Zygote is running mode.\n"
-		"	-z, --clear		Clear the device display.\n\n");
+		"	-V, --version:		Show version.\n"
+		"	-i, --info:		Print device display information.\n\n"
+		""
+		"	-z, --zygote:		Zygote mode.\n"
+		"	-p, --power:		Send power key event.\n"
+		"	-c, --clear		Clear the device display.\n\n"
+		""
+		"	-x:			X coordinate pixel [default 10]\n"
+		"	-y:			Y coordinate pixel [default 10]\n"
+		"	-b, --brightness:	Set display brightness [0-255] [default 170]\n"
+		"	-w, --boxwidth:		Text box column size [default 65]\n"
+		"	-f, --font:		Font name [.ttf file]\n"
+                "	-s, --size:		Font size [default 12]\n"
+		"	-t, --textcolor:	Font color [RGB Hex: FFFFFF]\n"
+		"	-a, --alpha:		Alpha channel [1-100]\n\n");
 }
 
 void version(void)
